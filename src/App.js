8852cb9e68facb4da322bce9e909206cb009c89e
Gabi Saito <gabrielasaitopereira@email.com>
2025-09-15 10:10:56 -0300

    feat: configuração de rotas e instalação do mui + routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Home from './Pages/Home';
+import Login from './Pages/Login';
+import Features from './Pages/Features';
+import NotFound from './Pages/NotFound';
+import CssBaseline from '@mui/material/CssBaseline';
+import { GlobalStyles } from '@mui/material';
 
 function App() {
+  return (
+    <>
+      <GlobalStyles/>
+      <CssBaseline />
+      <HashRouter>
+        <GAListener />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path='/ferramentas' element={<Features />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </HashRouter>
+    </>
+  );
 }
 
 export default App;
